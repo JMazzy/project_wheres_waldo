0699a20fc638cbf388273643ed5951b0e54e5cd4
Joshua Masland-Sarani <j.masland@gmail.com>
2016-03-21 10:12:33 -0400

    set up photo tag module

--- app/assets/javascripts/photo_tagger.js
+++ app/assets/javascripts/photo_tagger.js
@@ -1,10 +1,22 @@
-$( document ).ready( function() {
-  if ( $("#photos-show").length ) {
+var WALDO = WALDO || {};
+
+WALDO.photoTagModule = ( function() {
+  var registerListeners = function() {
     $('#photo').on("click", function(e) {
       var offset = $("#photo").offset();
       var x = e.pageX - offset.left
       var y = e.pageY - offset.top
       console.log(x,y);
     });
+  };
+
+  return {
+    registerListeners: registerListeners,
+  }
+})();
+
+$( document ).ready( function() {
+  if ( $("#photos-show").length ) {
+    WALDO.photoTagModule.registerListeners();
   }
 });
