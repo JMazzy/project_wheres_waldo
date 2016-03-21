4d72bc615fb81b15e6fe667b197be581ab7cdd04
Joshua Masland-Sarani <j.masland@gmail.com>
2016-03-21 13:06:57 -0400

    add tags... almost

--- app/assets/javascripts/photo_tagger.js
+++ app/assets/javascripts/photo_tagger.js
@@ -6,8 +6,27 @@ WALDO.photoTagModule = ( function() {
       var offset = $("#photo").offset();
       var x = e.pageX - offset.left
       var y = e.pageY - offset.top
-      console.log(x,y);
+      createTag(x,y);
     });
+  };
+
+  var createTag = function(x,y) {
+    // create new elements
+    var $newTag = $("<div class='tag'></div>");
+    var $tagBox = $("<div class='tag-box'></div>")
+    var $tagLabel = $("<div class='tag-label'></div>")
+    var $tagMenu = $("<div class='tag-menu'></div>")
+
+    //combine new elements under tag
+    $newTag.append($tagBox);
+    $newTag.append($tagLabel);
+    $newTag.append($tagMenu);
+
+    // put tag in position
+    $newTag.css({top: y, left: x});
+
+    // append new tag to the photo holder
+    $('#photo-holder').append($newTag);
   };
 
   return {
