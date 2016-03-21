060f742560812e014b6cb2a005e72d5b96d71112
Joshua Masland-Sarani <j.masland@gmail.com>
2016-03-21 08:51:56 -0400

    set up migrations

--- db/migrate/20160321123728_create_photos.rb
+++ db/migrate/20160321123728_create_photos.rb
@@ -1,6 +1,8 @@
 class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
+      t.string :title
+      t.string :link
 
       t.timestamps null: false
     end
--- db/migrate/20160321123744_create_characters.rb
+++ db/migrate/20160321123744_create_characters.rb
@@ -1,6 +1,9 @@
 class CreateCharacters < ActiveRecord::Migration
   def change
     create_table :characters do |t|
+      t.string :name
+
+      t.index :name, unique: true
 
       t.timestamps null: false
     end
