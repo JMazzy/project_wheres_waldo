39dd35e61439440f5c2eef423971a345ae13673f
Joshua Masland-Sarani <j.masland@gmail.com>
2016-03-21 10:07:56 -0400

    set up click coordinates

--- app/assets/javascripts/photo_tagger.js
+++ app/assets/javascripts/photo_tagger.js
@@ -1,5 +1,10 @@
 $( document ).ready( function() {
   if ( $("#photos-show").length ) {
-
+    $('#photo').on("click", function(e) {
+      var offset = $("#photo").offset();
+      var x = e.pageX - offset.left
+      var y = e.pageY - offset.top
+      console.log(x,y);
+    });
   }
 });
